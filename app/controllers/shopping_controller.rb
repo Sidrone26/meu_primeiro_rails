bfb5d7caaf58f169f58a46a1cacf2213b84732bd
Your Name <you@example.com>
2018-09-24 14:32:24 -0300

    Starting

---
+++ app/controllers/shopping_controller.rb
@@ -0,0 +1,17 @@
+class ShoppingController < ApplicationController
+  def new
+    @shop = shop.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
