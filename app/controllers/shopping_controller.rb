14b90147e680a42b7caf377b1379b07b7f44cb03
Your Name <you@example.com>
2018-09-27 14:29:52 -0300

    CRUD

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,12 +1,26 @@
 class ShoppingController < ApplicationController
-  def new
-    @shop = shop.new
+   def index
+    @shops = Shop.all
+  end
+  def index
+    @shops = Shop.all
+  end
+  def shows
+    @shop = Shop.find_by(id: params['id'])
   end
 
   def create
+    shop = Shop.new(permit)
+    if shop.save
+      @msg = 'Salvo com sucessso!'
+    else
+      @msg = shop.errors.messages
+    end
   end
 
   def edit
+      @shop = Shop.find_by(id: params['id'])
+
   end
 
   def update
@@ -14,4 +28,11 @@ class ShoppingController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def permit
+    params['shop'].permit(:name, :addres)
+  end
 end
+
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,4 @@
 class Shop < ApplicationRecord
+	validates :name , presence: {message: 'Preençha o nome'}
+	validates :addres , presence: {message: 'Informe seu endereço'}
 end
